018a351086aae42b70761a6e9a8a174bd94ffc23
Hugoch13 <hugojachb13@gmail.com>
2022-08-30 11:19:35 -0600

    prueba6

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Esto es una prueba de cambios desde la de hugo</Text>
+      <Text>Esto es una prueba de cambios desde la de hugo keiler</Text>
       <StatusBar style="auto" />
     </View>
   );
