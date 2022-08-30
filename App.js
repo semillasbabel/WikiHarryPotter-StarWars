76642776347d78144c2855ca85c51c835d9f02c2
Hugoch13 <hugojachb13@gmail.com>
2022-08-30 11:22:02 -0600

    cambuios2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Esto es una prueba de cambios desde la de hugo keiler</Text>
+      <Text>jajajajaja</Text>
       <StatusBar style="auto" />
     </View>
   );
