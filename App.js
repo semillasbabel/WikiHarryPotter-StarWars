8212a18890d59b79bbc4766bc3686f21fc935cfe
KeilerGB <keiler.cortes@grupobabel.com>
2022-08-30 11:23:23 -0600

    App Basica

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>jajajajaja</Text>
+      <Text>Volviendo al principio</Text>
       <StatusBar style="auto" />
     </View>
   );
