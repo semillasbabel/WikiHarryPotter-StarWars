53fa818e10d5ebf9aa2fa54d15a67ee33f58095a
KeilerGB <keiler.cortes@grupobabel.com>
2022-09-02 16:33:23 -0600

    Ventana MenuPrincipal Lista

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MainMenu from './Data/MainMenu'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Volviendo al principio</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <MainMenu/>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Data/MainMenu.js
@@ -0,0 +1,79 @@
+
+import React from "react";
+import { View, Image, TouchableOpacity} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HarryPotter from "./HarryPotter/Views/MenuHP";
+import StarWars from "./StarWars/Views/MenuSW";
+
+import {menuKeys} from "./MainMenuKeys";
+import {styles} from "./MainMenuStyles"
+
+function HomeScreen({navigation}) {
+    return (
+        <View style={styles.container}>
+            
+            <View style={styles.imagecontainer}>
+                <TouchableOpacity onPress={() => navigation.navigate(menuKeys.navegacionhp)}>
+                    <Image source={{uri: menuKeys.hpimage}}
+                    style={styles.image}/>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.imagecontainer}>
+                <TouchableOpacity  onPress={() => navigation.navigate(menuKeys.navegacionsw)}>
+                    <Image source={{uri: menuKeys.swimage}}
+                    style={styles.image}/>
+                </TouchableOpacity>
+            </View>
+
+        </View>
+    );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      
+      <Stack.Navigator
+      
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#9e9e9e"},
+            headerTintColor: '#E53935',
+          headerTitleStyle:{
+            fontSize: 25,
+            fontWeight: 'bold',
+          },
+          headerShown: false,
+          headerTitleAlign: 'center',
+        }}>
+
+        <Stack.Screen
+          name={menuKeys.navegacionmenu}
+          component={HomeScreen}
+          options={{}}
+        />
+
+        {/* <Stack.Screen 
+          name={menuKeys.navegacionhp} 
+          component={HarryPotter}
+          options={{title: 'Harry Potter'}}
+          
+        />
+
+        <Stack.Screen 
+          name={menuKeys.navegacionsw} 
+          component={StarWars}
+          options={{ title: 'StarWars'}}
+        /> */}
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
