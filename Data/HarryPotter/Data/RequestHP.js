b79e99d668644af88a2ca8d6c04cdeb2003fc232
KeilerGB <keiler.cortes@grupobabel.com>
2022-09-05 13:07:47 -0600

    Lista la Ventana HP

---
+++ Data/HarryPotter/Data/RequestHP.js
@@ -0,0 +1,80 @@
+import axios from 'axios';
+import {hpkeys} from './KeysHP';
+
+export async function SwitchSearchHP(SearchType){ /*Cambiar las funciones y variables a minuscula*/ 
+    let Path = "";
+
+    switch (SearchType) {
+
+        case hpkeys.ALLKEY:
+            Path = hpkeys.ALLPATH;
+            return await HPREQUESTAXIO(Path); 
+        break;
+
+        case hpkeys.STUDENTSKEY:
+            Path = hpkeys.STUDENTSPATH;
+            return await HPREQUESTAXIO(Path); 
+        break;
+
+        case hpkeys.STAFFKEY: 
+            Path = hpkeys.STAFFPATH;
+            return await HPREQUESTAXIO(Path); 
+        break;
+
+        case hpkeys.GryffindorKEY:
+            Path = hpkeys.GryffindorPATH;
+            return await HPREQUESTAXIO(Path); 
+        break;
+
+        case hpkeys.HufflepuffKEY:
+            Path = hpkeys.HufflepuffPATH; 
+            return await HPREQUESTAXIO(Path); 
+        break;
+
+        case hpkeys.RavenclawKEY:
+            Path = hpkeys.RavenclawPATH;
+            return await HPREQUESTAXIO(Path); 
+        break;
+
+        case hpkeys.SlytherinKEY:
+            Path = hpkeys.SlytherinPATH;
+            return await HPREQUESTAXIO(Path); 
+        break;
+
+    }
+
+}
+
+
+
+let TEMP = [];
+let RETURN = [];
+
+async function HPREQUESTAXIO(Path = ""){
+
+    await axios({
+        method: "get",
+        url: Path,
+    })
+    .then(res => {TEMP.push(res.data), vali = valiset()})
+    .catch((error) => {TEMP.push([{name:`${error.name}: ${error.message}`}])})
+    
+    if (RETURN.length > 0) {
+        RETURN = [];
+
+        TEMP.forEach(function(dato,index){
+            RETURN.push(TEMP[index]);
+        })
+    
+        TEMP = [];
+        return RETURN[0];
+
+    } else {
+        TEMP.forEach(function(dato,index){
+            RETURN.push(TEMP[index]);
+        })
+    
+        TEMP = [];
+        return RETURN[0];
+    }
+}
--- Data/HarryPotter/Views/MenuHP.js
+++ Data/HarryPotter/Views/MenuHP.js
@@ -8,18 +8,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import SearchComponent from '../Componentes/Searchlist';
 // import Houses from '../Componentes/HousesHP'
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import SearchComponent from "../Components/SearchList";
+import Listhouses from "../Components/ListHouse";
 
 
 
-// import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 // import { Button } from "@rneui/themed";
 
 // import Listacasas from "./listacasas"
 
 // import Lista1 from "./Personjeshp";
-// import Detalles from "./Details"
+import Details from "./Details"
 import {hpkeys} from "../Data/KeysHP"
 import { styles } from "../Styles/StylesheetHP";
+import listHouse from "../Components/ListHouse";
 
 // import { IconButton } from "react-native-paper";
 
@@ -37,7 +40,8 @@ export default class App extends Component {
           <Image source={{uri: hpkeys.logomenu}} style={styles.MenuHPimagelogo}/>
 
           <TouchableOpacity style={styles.MenuHPbntgoback} onPress={() => { this.props.navigation.goBack() }}>
-              <Text style={styles.MenuHPiconogoback}>🏠</Text>
+              {/* <Text style={styles.MenuHPiconogoback}>🏠</Text> */}
+              <Ionicons name="arrow-back" size={30} style={styles.MenuHPiconback} />
           </TouchableOpacity>
 
         </View>
@@ -56,8 +60,8 @@ export default class App extends Component {
                   size = 30
                 }
                 
-                if (route.name === hpkeys.TabEstudents) {
-                iconName = hpkeys.Estudentsicon;
+                if (route.name === hpkeys.TabStudents) {
+                iconName = hpkeys.Studentsicon;
                 size = 30
                 }
                 
@@ -82,10 +86,10 @@ export default class App extends Component {
               tabBarActiveTintColor: '#e8b800',
               tabBarInactiveTintColor: '#7f0000'
           })}>
-            <Tab.Screen name={hpkeys.TabCharacteres} component={PersonjesScreen} />
-            <Tab.Screen name={hpkeys.TabEstudents} component={EstudiantesScreen} />
+            <Tab.Screen name={hpkeys.TabHouses} component={HousesScreen} />
+            <Tab.Screen name={hpkeys.TabCharacteres} component={CharactersScreen} />
+            <Tab.Screen name={hpkeys.TabStudents} component={StudiantsScreen} />
             <Tab.Screen name={hpkeys.TabStaff} component={StaffScreen} />
-            <Tab.Screen name={hpkeys.TabHouses} component={CasasScreen} />
           </Tab.Navigator>
         </View>
 
@@ -107,183 +111,131 @@ export default class App extends Component {
 
 
 
-
-
-
-
-
-
-
-
-
-// function DetailsScreen({ navigation }) {
-//   const navi = useNavigation();
-//   return <Listacasas {...navigation} navigation={navi} />;
-// }
-
-
-// const SettingsStack = createNativeStackNavigator();
-
-// function SettingsStackScreen() {
-//   return (
-//     <SettingsStack.Navigator>
-//       <SettingsStack.Screen name="Settings" component={SettingsScreen} />
-//       <SettingsStack.Screen name="Details" component={DetailsScreen} />
-//     </SettingsStack.Navigator>
-//   );
-// }
-
-
-
-
-// function Busqueda1({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, backgroundColor: "black" }}>
-//       <SearchComponent saludo="All" Urlenv="http://hp-api.herokuapp.com/api/characters" />
-//     </View>
-//   )
-// }
-
-// // function Busqueda2(){
-// //   return(
-// //     <View style={{ flex: 1, backgroundColor: "black"}}>
-// //       <SearchComponent saludo="All" Urlenv = "http://hp-api.herokuapp.com/api/characters/students"/>
-// //     </View>
-// //   )
-// // }
-
-// function Busqueda2({ navigation }) {
-//   const navi = useNavigation();
-//   return <SearchComponent {...navigation} navigation={navi} />;
-// }
-
-// function Busqueda3({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, backgroundColor: "black" }}>
-//       <SearchComponent saludo="All" Urlenv="http://hp-api.herokuapp.com/api/characters/staff" />
-//     </View>
-//   )
-// }
-
-// function Busqueda4({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, backgroundColor: "black" }}>
-//       <SearchComponent saludo="All" Urlenv="http://hp-api.herokuapp.com/api/characters/house/gryffindor" />
-//     </View>
-//   )
-// }
-// function Casa({ navigation }) {
-//   return (
-//     <View style={{ flex: 1 }}>
-
-//       <View style={{ flex: 1, backgroundColor: "red" }}>
-//         <TouchableOpacity onPress={() => navigation.navigate('List')}>
-//           <Image
-//             source={{
-//               uri: "https://p4.wallpaperbetter.com/wallpaper/672/923/822/harry-potter-gryffindor-wallpaper-preview.jpg"
-//             }}
-//             style={{ height: '100%', width: '100%' }}
-//           />
-//         </TouchableOpacity>
-
-//       </View>
-
-//       <View style={{ flex: 1, backgroundColor: "green" }}>
-//         <TouchableOpacity onPress={() => navigation.navigate('List')}>
-//           <Image
-//             source={{
-//               uri: "https://wallpapers.com/images/hd/harry-potter-slytherin-snake-crest-w0yy2ufprp0rcn24.webp"
-//             }}
-//             style={{ height: '100%', width: '100%' }}
-//           />
-//         </TouchableOpacity>
-//       </View>
-
-//       <View style={{ flex: 1, backgroundColor: "blue" }}>
-//       <TouchableOpacity onPress={() => navigation.navigate('List')}>
-//           <Image
-//             source={{
-//               uri: "https://wallpapers.com/images/featured/ravenclaw-0biazn74ja08p3un.jpg"
-//             }}
-//             style={{ height: '100%', width: '100%' }}
-//           />
-//         </TouchableOpacity>
-//       </View>
-
-//       <View style={{ flex: 1, backgroundColor: "yellow" }}>
-//       <TouchableOpacity onPress={() => navigation.navigate('List')}>
-//           <Image
-//             source={{
-//               uri: "https://wallpaper.dog/large/510701.jpg"
-//             }}
-//             style={{ height: '100%', width: '100%' }}
-//           />
-//         </TouchableOpacity>
-//       </View>
-
-//     </View>
-//   )
-
-// }
-
-function vacio(){
-    return(
-        <View>
-            <Text>
-                Holaa
-            </Text>
-        </View>
-    )
-}
-
-const Personajes = createNativeStackNavigator();
-
-function PersonjesScreen({ navigation }) {
+const Characters = createNativeStackNavigator();
+function CharactersScreen() {
   return (
-    <Personajes.Navigator screenOptions={{ headerShown: false }}>
-
-      <Personajes.Screen name="Home" component={vacio} />
-      <Personajes.Screen name="Details" component={vacio} />
-    </Personajes.Navigator>
+    <Characters.Navigator screenOptions={{ headerShown: false }}>
+      <Characters.Screen name="Home" component={SearchAll} />
+      <Characters.Screen name="Details" component={Details} />
+    </Characters.Navigator>
   );
 }
 
-const Estudiantes = createNativeStackNavigator();
-
-function EstudiantesScreen({ navigation }) {
+const Students = createNativeStackNavigator();
+function StudiantsScreen() {
   return (
-    <Estudiantes.Navigator screenOptions={{ headerShown: false }}>
-
-      <Estudiantes.Screen name="Home" component={vacio} />
-      <Estudiantes.Screen name="Details" component={vacio} />
-    </Estudiantes.Navigator>
+    <Students.Navigator screenOptions={{ headerShown: false }}>
+      <Students.Screen name="Home" component={SearchStudents} />
+      <Students.Screen name="Details" component={Details} />
+    </Students.Navigator>
   );
 }
 
 const Staff = createNativeStackNavigator();
-
-function StaffScreen({ navigation }) {
+function StaffScreen() {
   return (
     <Staff.Navigator screenOptions={{ headerShown: false }}>
-      <Staff.Screen name="Home" component={vacio} />
-      <Staff.Screen name="Details" component={vacio} />
+      <Staff.Screen name="Home" component={Searchstaff} />
+      <Staff.Screen name="Details" component={Details} />
     </Staff.Navigator>
   );
 }
 
-const Casas = createNativeStackNavigator();
-
-function CasasScreen({ navigation }) {
+const Houses = createNativeStackNavigator();
+function HousesScreen({ navigation }) {
   return (
-    <Casas.Navigator screenOptions={{ headerShown: false }}>
-      <Casas.Screen name="Home" component={vacio} />
-      <Casas.Screen name="List" component={vacio} />
-      <Casas.Screen name="Details" component={vacio} />
-    </Casas.Navigator>
+    <Houses.Navigator screenOptions={{ headerShown: false }}>
+      <Houses.Screen name="Home" component={Searchhouses} />
+      <Houses.Screen name="List" component={Listhouses} />
+      <Houses.Screen name="Details" component={Details} />
+    </Houses.Navigator>
   );
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function SearchAll({navigation}){
+  const navi = useNavigation();
+  return <SearchComponent {...navigation} SearchType={hpkeys.ALLKEY} navigation={navi}/>;
+}
+
+function SearchStudents({navigation}){
+  const navi = useNavigation();
+  return <SearchComponent {...navigation} SearchType={hpkeys.STUDENTSKEY} navigation={navi}/>;
+}
+
+function Searchstaff({navigation}){
+  const navi = useNavigation();
+  return <SearchComponent {...navigation} SearchType={hpkeys.STAFFKEY} navigation={navi}/>;
+}
+
+function Searchhouses({ navigation }) {
+  return (
+    <View style={styles.MenuHPHousesContainer}>
+
+      <View style={styles.MenuHPHouseContainer}>
+
+        <TouchableOpacity onPress={() => navigation.navigate('List', {house: hpkeys.GryffindorKEY})}>
+          <Image
+            source={{uri: hpkeys.Gryffindorimage}}
+            style={styles.MenuHPHousesImage}
+          />
+        </TouchableOpacity>
+
+      </View>
+
+      <View style={styles.MenuHPHouseContainer}>
+
+        <TouchableOpacity onPress={() => navigation.navigate('List', {house: hpkeys.HufflepuffKEY})}>
+          <Image
+            source={{uri: hpkeys.Hufflepuffimage}}
+            style={styles.MenuHPHousesImage}
+          />
+        </TouchableOpacity>
+
+      </View>
+
+      <View style={styles.MenuHPHouseContainer}>
+
+        <TouchableOpacity onPress={() => navigation.navigate('List', {house: hpkeys.RavenclawKEY})}>
+          <Image
+            source={{uri: hpkeys.Ravenclawimage}}
+            style={styles.MenuHPHousesImage}
+          />
+        </TouchableOpacity>
+
+      </View>
+
+      <View style={styles.MenuHPHouseContainer}>
+
+        <TouchableOpacity onPress={() => navigation.navigate('List', {house: hpkeys.SlytherinKEY})}>
+          <Image
+            source={{uri: hpkeys.Slytherinimage}}
+            style={styles.MenuHPHousesImage}
+          />
+        </TouchableOpacity>
+
+      </View>
+
+    </View>
+  )
+
+}
 
 
 
