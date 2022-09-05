00e73b9b9ed9ba96cf775a9e93d3322f2ef4f61d
Hugoch13 <hugojachb13@gmail.com>
2022-09-05 08:55:03 -0600

    prueba

---
+++ Data/StarWars/Components/SearchPlanets.js
@@ -0,0 +1,65 @@
+import React,{Component} from 'react';
+import { Text, View, SafeAreaView, FlatList, Alert, ScrollView, TouchableOpacity, ActivityIndicator, ImageBackground} from 'react-native';
+import axios, { AxiosError } from 'axios';
+import Imagelist from './imagenlist.js';
+// import {SwitchSearchSW} from "../Data/RequestSW";
+
+import {styles} from "../Estilos/Estiloswars";
+import { Card } from '@rneui/themed';
+import { BlurView, VibrancyView } from "@react-native-community/blur";
+
+export default class CharactersComponent extends Component{
+
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    Busqueda: [],
+    loading: true,
+  }
+
+  async componentDidMount() {
+    const {SearchType} = this.props;
+
+    await axios({
+        method: "get",
+        url: `https://swapi.dev/api/planets/`,
+    })
+    .then(res => this.setState({Busqueda: res.data.results, loading: false}));
+  }
+
+  renderItem(data){
+    return( 
+    <ScrollView> 
+      <View style={{backgroundColor: "white", borderRadius: 15,opacity: 0.6, height: 150, marginHorizontal: 20, marginVertical: 20 }}>
+      <Text style={styles.textview}>Pelicula: {data.item.name}</Text>
+      <Text style={styles.textview}>Director De Pelicula{data.item.rotation}</Text>
+      <Text style={styles.textview}>Pelicula #: {data.item.climate}</Text>
+      <Text style={styles.textview}>Fecha de Salida:{data.item.terrain}</Text>
+      <Text style={styles.textview}>Fecha de Salida:{data.item.population}</Text>
+      </View>
+     
+    </ScrollView>
+    )
+  }
+
+  render(){
+    const {Busqueda, loading} = this.state;
+    const {navigation} = this.props;
+    if(!loading){
+      return (
+        <SafeAreaView>
+          <FlatList
+            data={Busqueda}
+            renderItem={this.renderItem}
+          />
+        </SafeAreaView>
+      )
+    }
+    else{
+      return <ActivityIndicator size="large" color="#white"/>
+    }
+  }
+
+}
