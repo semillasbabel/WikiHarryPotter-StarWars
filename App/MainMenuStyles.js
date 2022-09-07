f41339dda5578cc08bc450d16dafdc51d5080a5a
Hugoch13 <hugojachb13@gmail.com>
2022-09-07 10:20:03 -0600

    cambio   starwars colors

---
+++ App/MainMenuStyles.js
@@ -0,0 +1,24 @@
+import { StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        
+    },
+
+    imagecontainer: {
+        flex: 1,
+        blurRadius:10, 
+        backgroundColor: "#0091EA", 
+        width: '100%', 
+        height: '100%',  
+        justifyContent: 'center', 
+        alignContent: 'center',
+        
+    },
+
+    image: {
+        height: '100%', 
+        width: '100%'
+    }
+});
---
+++ App/StarWars/Views/Detailsmovie.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {Text, Image, View, Button, TouchableOpacity, ScrollView} from 'react-native';
+import { styles } from "../Styles/StylesheetSW";
+import { Card, Icon, Button } from "@rneui/themed";
+import { color } from "@rneui/base";
+
+
+function DetailsPlanets({ route, navigation }) {
+  const {item} = route.params;
+  return (
+
+    <View style={{flex: 1, backgroundColor: "gray"}}>
+
+        <TouchableOpacity style={styles.btnBack2} onPress={() => navigation.goBack()}>
+            <Text style={styles.textback2}>Volver</Text>
+            
+        </TouchableOpacity>
+        
+        <ScrollView> 
+          <TouchableOpacity>
+      <View style={{backgroundColor: "white", borderRadius: 15,opacity: 0.6, height: 150, marginHorizontal: 20, marginVertical: 20 }}>
+      <Text style={styles.textview}>Pelicula: {data.item.title}</Text>
+      <Text style={styles.textview}>Director De Pelicula{data.item.director}</Text>
+      <Text style={styles.textview}>Pelicula #: {data.item.episode_id}</Text>
+      <Text style={styles.textview}>Fecha de Salida:{data.item.release_date}</Text>
+     
+      </View>
+    
+      </TouchableOpacity>
+      {/* <Card>
+        <Card.Title style={styles.cardtext2}>{data.item.title}</Card.Title>
+        <Card.Divider style={styles.divider2}/>
+      <Text>Director De Pelicula{data.item.director}</Text>
+      <Text>Pelicula #: {data.item.episode_id}</Text>
+      <Text>Fecha de Salida:{data.item.release_date}</Text>
+     
+    </Card> */}
+    </ScrollView>
+        
+    </View>
+  );
+}
+
+export default DetailsPlanets;
